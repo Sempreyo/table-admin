55fa2672afda38809fae5f0b47f0558901e1dcf8
ArtZ <snickgem@gmail.com>
2025-06-23 23:01:11 +0300

    feature: import xlsx, remove row/column, column width

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -208,13 +208,6 @@ const createTable = (parent, rowsInput, columnsInput, menuCreate, menuPanel) => 
 
 	menuCreate.setAttribute("hidden", true);
 	menuPanel.removeAttribute("hidden");
-
-	// Создание событий
-	/*const newTable = parent.querySelector("table");
-
-	newTable.addEventListener("click", (e) => {
-
-	});*/
 }
 
 const makeCellEditable = (td) => {
@@ -264,6 +257,15 @@ const createRowHandler = (table) => {
 	}
 }
 
+const removeRowHandler = (table) => {
+	const focusedCell = table.querySelector(".focused");
+
+	if (focusedCell) {
+		const row = focusedCell.closest("tr");
+		row.remove();
+	}
+}
+
 const createColumnHandler = (table) => {
 	const focusedCellIndex = table.querySelector(".focused").cellIndex;
 
@@ -276,6 +278,15 @@ const createColumnHandler = (table) => {
 		setEditableCellEvent(newCell, table);
 
 		cellSelected.insertAdjacentElement("afterend", newCell);
+	});
+}
+
+const removeColumnHandler = (table) => {
+	const focusedCellIndex = table.querySelector(".focused").cellIndex;
+
+	table.querySelectorAll("tr").forEach(tr => {
+		const cellSelected = tr.querySelectorAll("td, th")[focusedCellIndex];
+		cellSelected.remove();
 	});
 }
 
@@ -359,6 +370,15 @@ const dropdownSubmitHandler = (table, form, event, dropdownType) => {
 					break;
 			}
 			break;
+		case "width":
+			const widthFieldValue = form.querySelector('[name="width"]').value;
+
+			table.querySelectorAll("tr").forEach(tr => {
+				const cellSelected = tr.querySelectorAll("td, th")[focusedCell.cellIndex];
+
+				cellSelected.setAttribute("width", widthFieldValue);
+			});
+			break;
 	}
 	form.setAttribute("hidden", "true");
 	form.reset();
@@ -366,9 +386,162 @@ const dropdownSubmitHandler = (table, form, event, dropdownType) => {
 
 const exportToXLSX = (table, filename) => {
 	if (table) {
-		const book = XLSX.utils.table_to_book(table);
+		// Создаем клон таблицы без скрытых объединением ячеек
+		const tableClone = table.cloneNode(true);
+		const rows = tableClone.querySelectorAll("tr");
+
+		rows.forEach(row => {
+			const cells = row.querySelectorAll("td, th");
+
+			cells.forEach(cell => {
+				if (cell.hasAttribute("hidden")) {
+					cell.remove();
+				}
+			});
+		});
+
+		// Преобразуем таблицу в книгу Excel и пишем в файл
+		const book = XLSX.utils.table_to_book(tableClone);
 		XLSX.writeFile(book, filename);
 	}
+}
+
+const importFromXLSX = (table, target) => {
+	if (!table) return;
+
+	// Добавляем события ячейкам импортированной таблицы
+	const setEventsToImportedTable = (cell, rowIndex, columnIndex) => {
+		if (rowIndex === 1 && columnIndex === 0) {
+			setCellFocus(cell, table);
+		}
+
+		if (rowIndex === 0) {
+			cell.dataset.type = "th";
+			cell.dataset.row = "0";
+		} else {
+			cell.dataset.row = `${rowIndex - 1}`;
+		}
+		cell.dataset.column = columnIndex.toString();
+
+		setEditableCellEvent(cell, table);
+		cell.addEventListener("mouseenter", (e) => updateSelectionHandler(table, e.target));
+	}
+
+	const file = target.files[0];
+	const errorMessage = target.nextElementSibling;
+
+	if (!file) {
+		errorMessage.textContent = "Файл не выбран";
+		errorMessage.removeAttribute("hidden");
+		return;
+	} else {
+		errorMessage.setAttribute("hidden", "true");
+	}
+
+	const reader = new FileReader();
+
+	reader.onload = (e) => {
+		const data = new Uint8Array(e.target.result);
+		const book = XLSX.read(data, {type: "array"});
+
+		// Получаем данные из первого листа
+		const firstSheetName = book.SheetNames[0];
+		const sheet = book.Sheets[firstSheetName];
+		const jsonData = XLSX.utils.sheet_to_json(sheet, {header: 1});
+
+		// Получаем данные об объединенных ячейках
+		const merges = sheet["!merges"] || [];
+		const mergeObj = {};
+
+		// Формируем объект объединенных ячеек
+		merges.forEach(merge => {
+			const mainKey = `${merge.s.r},${merge.s.c}`;
+
+			mergeObj[mainKey] = {
+				rowspan: merge.e.r - merge.s.r + 1,
+				colspan: merge.e.c - merge.s.c + 1,
+				hiddenCells: []
+			}
+
+			for (let r = merge.s.r; r <= merge.e.r; r++) {
+				for (let c = merge.s.c; c <= merge.e.c; c++) {
+					if (r !== merge.s.r || c !== merge.s.c) {
+						mergeObj[mainKey].hiddenCells.push(`${r},${c}`);
+					}
+				}
+			}
+		});
+
+		// Заменяем содержимое таблицы
+		table.innerHTML = "";
+
+		if (!jsonData || jsonData.length === 0) return;
+
+		resetSelection();
+
+		const thead = document.createElement("thead");
+		const tbody = document.createElement("tbody");
+
+		// Находим максимальную длину строки
+		const maxCellsLength = jsonData.find(arr => arr.length === Math.max(...jsonData.map(data => data.length))).length;
+
+		jsonData.forEach((rowData, rowIndex) => { // Перебор строк
+			const row = document.createElement("tr");
+
+			for (let columnIndex = 0; columnIndex < maxCellsLength; columnIndex++) { // Перебор ячеек с строке
+				const cellData = (rowData || [])[columnIndex];
+				const cellKey = `${rowIndex},${columnIndex}`;
+				let isMainCell = false;
+
+				// Проверяем главную для объединения ячейку
+				for (const key in mergeObj) {
+					if (cellKey === key) {
+						isMainCell = true;
+						break;
+					}
+				}
+
+				// Создаем главную для объединения ячейку
+				if ((cellData !== undefined) && (cellData !== null)) {
+					const cell = document.createElement(rowIndex === 0 ? "th" : "td");
+					cell.textContent = cellData.toString();
+
+					if (isMainCell) {
+						cell.rowSpan = mergeObj[cellKey].rowspan;
+						cell.colSpan = mergeObj[cellKey].colspan;
+						cell.setAttribute("data-merged-main", "true");
+						row.append(cell);
+					} else {
+						row.append(cell);
+					}
+
+					setEventsToImportedTable(cell, rowIndex, columnIndex);
+				} else {
+					const hiddenCell = document.createElement(rowIndex === 0 ? "th" : "td");
+					hiddenCell.setAttribute("data-merged", "true");
+
+					// Находим индекс строки и столбца главной ячейки, к которой относится скрытая
+					const searchMergedCell = Object.keys(mergeObj).find(key => mergeObj[key.toString()].hiddenCells.includes(cellKey));
+
+					hiddenCell.setAttribute(
+						"data-merged-with",
+						`${(rowIndex === 0) ? searchMergedCell.split(",")[0] : searchMergedCell.split(",")[0] - 1}, ${searchMergedCell.split(",")[1]}`
+					);
+					hiddenCell.setAttribute("hidden", "true");
+					row.append(hiddenCell);
+
+					setEventsToImportedTable(hiddenCell, rowIndex, columnIndex);
+				}
+			}
+
+			(rowIndex === 0) ? thead.append(row) : tbody.append(row);
+		});
+
+		table.append(thead);
+		table.append(tbody);
+	};
+
+	reader.readAsArrayBuffer(file);
 }
 
 const resetSelection = () => {
@@ -391,12 +564,14 @@ document.addEventListener("DOMContentLoaded", () => {
 	const createButton = menuCreateTable.querySelector(".js-create-table");
 	const addRowButton = menuPanel.querySelector(".js-create-row");
 	const addColumnButton = menuPanel.querySelector(".js-create-column");
+	const removeRowButton = menuPanel.querySelector(".js-remove-row");
+	const removeColumnButton = menuPanel.querySelector(".js-remove-column");
 	const joinCellsButton = menuPanel.querySelector(".js-join-cells");
 	const splitCellsButton = menuPanel.querySelector(".js-split-cells");
 	const editButtons = menuPanel.querySelectorAll(".js-edit");
 	const dropButtons = menuPanel.querySelectorAll('[data-dropdown]');
 	const dropdowns = menuPanel.querySelectorAll(".table__dropdown");
-	const importButton = menuPanel.querySelector(".js-import");
+	const importField = menuPanel.querySelector(".js-import");
 	const exportButton = menuPanel.querySelector(".js-export");
 
 	createButton.addEventListener("click", () => {
@@ -406,6 +581,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	addRowButton.addEventListener("click", () => createRowHandler(table));
 	addColumnButton.addEventListener("click", () => createColumnHandler(table));
+	removeRowButton.addEventListener("click", () => removeRowHandler(table));
+	removeColumnButton.addEventListener("click", () => removeColumnHandler(table));
 	joinCellsButton.addEventListener("click", () => {
 		selection.active = true;
 	});
@@ -427,6 +604,7 @@ document.addEventListener("DOMContentLoaded", () => {
 			`import_${((new Date()).getDate()).toString().padStart(2, '0')}_${((new Date()).getMonth() + 1).toString().padStart(2, '0')}_${(new Date()).getFullYear()}.xlsx`
 		);
 	});
+	importField.addEventListener("change", (e) => importFromXLSX(table, e.target));
 
 	// Клик вне элемента - закрываем его
 	document.addEventListener("click", (e) => {
